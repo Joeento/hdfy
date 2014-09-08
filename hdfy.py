65c810183fb41115e8052e2af0ca619339cdc7c4
Eric Kudler <joeento@gmail.com>
2014-09-07 21:24:33 -0400

    Switched to opencv

--- hdfy.py
+++ hdfy.py
@@ -1,6 +1,13 @@
-from PIL import Image
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
+img = cv2.imread('pics/donottouch.jpg',0)
+edges = cv2.Canny(img,100,200)
 
-im = Image.open("pics/donottouch.jpg")
-pixels = im.load()
-print(pixels[0,0])
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
